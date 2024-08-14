f10582b3cb5ec854b70f2555f6cf01acaa09f4ca
21MoududAhammed <21moudud@gmail.com>
2024-08-14 18:38:31 +0600

    write and read operation have done

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
-const express = require('express');
-const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
 const port = process.env.PORT || 5000;
 
-// middleware 
+// middleware
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("This is backend for crud operation.");
+});
 
-app.get('/', (req, res)=>{
-    res.send('This is backend for crud operation.')
-})
+const uri =
+  "mongodb+srv://moududfkpirisda:wIl8MZZFd7hJfyKC@cluster0.eukaa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port, ${port}`);
-})
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
+    const booksCollection = client.db("booksDB").collection("books");
+
+    //post or insert a book details
+    app.post("/books", async (req, res) => {
+      const bookDetails = req.body;
+      //   console.log(JSON.stringify(book))
+      const result = await booksCollection.insertOne(bookDetails);
+      res.send(result);
+    });
+    //    get all books details
+    app.get("/books", async (req, res) => {
+      const cursor = booksCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server is running on port, ${port}`);
+});
