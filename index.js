3bc6561f4d36c97d6ccfe439b73b6e7c19347e10
21MoududAhammed <21moudud@gmail.com>
2024-08-14 11:20:05 +0600

    Initial setup has done

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express()
+
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res)=>{
+    res.send('This is backend for crud operation.')
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port, ${port}`);
+})
+
